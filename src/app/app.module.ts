b8f952f9a2bf62fd0b71a959c298f8ea4a387289
ashok bhati <ashok.bhati@sifars.com>
2024-04-08 20:49:54 +0530

    add routing support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { AppRoutes } from './app.routing';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(AppRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
