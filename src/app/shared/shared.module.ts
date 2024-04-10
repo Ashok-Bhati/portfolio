f778c28984d8230d2d6df8c8f21a4491f3558abd
ashok bhati <maharajatechnical@gmail.com>
2024-04-10 10:14:24 +0530

    Add social links

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { BasicDetailsComponent } from './components/basic-details/basic-details.component';
+import { SocialLinksComponent } from './components/social-links/social-links.component';
 
 @NgModule({
-  declarations: [HeaderComponent, BasicDetailsComponent],
+  declarations: [HeaderComponent, BasicDetailsComponent, SocialLinksComponent],
   imports: [CommonModule],
   exports: [HeaderComponent, BasicDetailsComponent],
 })
