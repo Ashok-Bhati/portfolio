e1352ad70a494c4db97faac89ab15a5f98448b94
ashok bhati <maharajatechnical@gmail.com>
2024-04-14 21:42:51 +0530

    add introduction section

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { BasicDetailsComponent } from './components/basic-details/basic-details.component';
 import { SocialLinksComponent } from './components/social-links/social-links.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
 
 @NgModule({
-  declarations: [HeaderComponent, BasicDetailsComponent, SocialLinksComponent],
+  declarations: [
+    HeaderComponent,
+    BasicDetailsComponent,
+    SocialLinksComponent,
+    IntroductionComponent,
+  ],
   imports: [CommonModule],
-  exports: [HeaderComponent, BasicDetailsComponent],
+  exports: [HeaderComponent, BasicDetailsComponent, IntroductionComponent, SocialLinksComponent],
 })
 export class SharedModule {}
